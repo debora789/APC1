c5e716ace67efe99122b11d6e7d03ebfdf4db337
Débora <debora.teixeira@iesb.edu.com.br>
2025-04-16 21:56:37 -0300

    Adicionando arquivos da pratica01

---
+++ aulas/aula05/aula06/numero_par.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main() {
+    int numero = 0;
+
+    printf("Entre com um numero inteiro: ");
+    scanf("%i", &numero);
+
+    int numero_eh_divisivel_por_2 = numero % 2 == 0;
+
+    if (numero_eh_divisivel_por_2) {
+    printf("O numero %i eh par!\n", numero);
+   } else {
+   printf("O numero %i eh impar!\n", numero);
+   }
+    return 0;
+}
